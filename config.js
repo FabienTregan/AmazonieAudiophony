0da8c8d13c48e5f2261850ad0876d7a56336b0e6
Fabien Tregan <fabien@treXXgan.fr>
2019-04-17 09:02:02 +0200

    Adds missing animals pictures and updates data about them.

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {
 
     // if set to 'true', will hide the mouse pointer (usefull on touchscreens)
-    hideMouse: true,
+    hideMouse: false,
 
     // time in seconds before the screensaver appears. If 0, screensaver is disabled.
     screensaverDelay : 240,
